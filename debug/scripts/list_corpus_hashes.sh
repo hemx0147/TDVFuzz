91ebaee8cd2e67b1cf5244fe6bec40852987a68f
hemx0147 <rnx781@gmail.com>
2022-11-04 14:08:33 +0100

    list corpus: adapt script to new repo structure & script format

--- debug/scripts/list_corpus_hashes.sh
+++ debug/scripts/list_corpus_hashes.sh
@@ -1,45 +1,105 @@
-#!/bin/bash
+#!/bin/env bash
 
-###
-# Lists corpi and their matching hashes.
-# Script must be run from within kAFL environment.
-# Note that this script may take a while to finish. This is due to mcat.py being slow.
-# 
-# @param 1 workdir	The kAFL working directory including a metadata/ folder
+##
+# List kAFL corpora and their matching hashes.
+# This script must be run from within the kAFL environment.
 #
-# @return A list of format <corpus_name>: <corpus_hash>
-###
+# Usage: list_corpus_hashes.sh [OPTION]
+#
+# Options:
+#   -h, --help    Display this help text
+#   -w WORKDIR    The kAFL working directory with logs/, metadata/ and corpus/
+#                 directories (default: KAFL_WORKDIR)
+##
+# This script parses the metadata information from the WORKDIR metadata/
+# directory to find matching corpora in the corpus/ directory.
+# The procedure may take a few minutes to finish since it's using the mcat.py
+# tool to parse the metadata information.
+##
 
 
-function fail {
-	echo
-	echo -e "$1"
-	echo
-	echo -e "Usage:\n\t$0 <kafl_workdir>"
-	echo
-	exit 1
+####################################
+# Global Variables
+####################################
+WORKDIR=$KAFL_WORKDIR
+VERBOSE=0
+
+####################################
+# Function Definitions
+####################################
+
+# print help text given above in comments
+function help()
+{
+    # find help-start line (indicated by two '#')
+    help_start="`grep -n "^##" "$0" | head -n 1 | awk -F ":" '{print $1}'`"
+    # print only help part
+    tail -n +"$help_start" "$0" | sed -ne '/^#/!q;s/.\{1,2\}//;1d;p'
+    exit
+}
+
+# print script usage information given above in comments
+function usage()
+{
+    # find usage line
+    usage_start=$(grep -n "^# Usage" "$0" | awk -F ":" '{print $1}')
+    # print only usage part
+    tail -n +"$usage_start" "$0" | sed -ne '/^#/!q;/^##/q;s/.\{1,2\}//;p'
+    exit
+}
+
+# exit with errorcode 1 & print usage.
+# optional argument: print error message.
+function fatal()
+{
+    [[ -n "$1" ]] && echo "Error: $1"; echo
+    usage >&2
+    exit 1
 }
 
 
-########## MAIN CODE ##########
+####################################
+# Main()
+####################################
+
+# argument parsing
+POSITIONAL_ARGS=()
+while [[ "$#" -gt 0 ]]
+do
+    case "$1" in
+        '-h'|'--help')
+            help
+            ;;
+        '-w')
+            [[ -z "$2" ]] && fatal "Missing parameter WORKDIR"
+            WORKDIR="$2"
+        	shift   # past argument
+            shift   # past value
+            ;;
+        -*|--*)
+            fatal "Unknown option $1"
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+            shift   # past argument
+            ;;
+    esac
+done
+set -- "${POSITIONAL_ARGS[@]}"  # restore positional parameters
 
 # test if in KAFL environment
-[[ -z "$BKC_ROOT" ]] && fail "Could not find BKC_ROOT. Verify that kAFL environment is set up."
-[[ -z "$KAFL_ROOT" ]] && fail "Could not find KAFL_ROOT. Verify that kAFL environment is set up."
+[[ -z "$BKC_ROOT" ]] && fatal "Could not find BKC_ROOT. Verify that kAFL environment is set up."
+[[ -z "$KAFL_ROOT" ]] && fatal "Could not find KAFL_ROOT. Verify that kAFL environment is set up."
 
-# command line parsing
-[[ $# -eq 1 ]] || fail "Missing arguments."
 
-KAFL_WORKDIR=$(realpath $1)	# kAFL work dir with logs/, corpus/ & metdadata/ folders
-
-METADATA=$KAFL_WORKDIR/metadata
-
-[[ -d $METADATA ]] || fail "Could not find metadata/ folder in workdir $KAFL_WORKDIR."
+METADATA=$WORKDIR/metadata
+[[ -d $WORKDIR ]] || fatal "Could not find kafl working directory $WORKDIR"
+[[ -d "$METADATA" ]] || fatal "Could not find metadata/ folder in $WORKDIR."
 
 # extract readable metadata from metadata files & match corpus hash to node
 for node in $(ls $METADATA)
 do
 	PAYLOAD=$(echo $node | sed 's/node/payload/')
 	CORPUS_HASH=$(mcat.py $METADATA/$node | grep "hash" | cut -c 20-35)
-	echo "$PAYLOAD: $CORPUS_HASH"
+	echo "$PAYLOAD $CORPUS_HASH"
 done
