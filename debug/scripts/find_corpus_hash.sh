49a348495bc7761ae283dec924b7f50f37cf9ddf
hemx0147 <rnx781@gmail.com>
2022-11-04 13:33:35 +0100

    find corpus: update to new repo structure & script format

--- debug/scripts/find_corpus_hash.sh
+++ debug/scripts/find_corpus_hash.sh
@@ -1,82 +1,162 @@
-#!/bin/bash
+#!/bin/env bash
 
-###
-# Find a matching corpus file for a given findings file (crash/kasan/timeout) that has a part of the corpus hash in its filename.
-# Script must be run from within kAFL environment.
-# Note that this script may take a while to finish. This is due to mcat.py being slow.
-# Note also that this script is very similar to list_corpus_hashes script but uses an early exit strategy to find a desired payload faster.
-# 
-# @param 1 workdir			The kAFL working directory
+##
+# Find a kAFL corpus that produced a crash/kasan/timeout in a kAFL session.
+# This script must be run from within the kAFL environment.
 #
-# @param 2 findings_file	Findings file with part of corpus hash in filename,
-#							e.g. kasan_<hash>.log or crash_<hash>.log
+# Usage: find_corpus.sh [OPTION] FILE
 #
-# @return corpus_name		Return the name of the corpus file whose hash matches the partial hash in the findings file name
-#							e.g. node_<number>
-###
+# Options:
+#   -h, --help    Display this help text
+#   -w WORKDIR    The kAFL working directory with logs/, metadata/ and corpus/
+#                 directories (default: KAFL_WORKDIR)
+#
+# Parameters:
+#   FILE    A kAFL findings file with part of corpus hash in filename,
+#           e.g. kasan_<hash>.log or crash_<hash>.log
+##
+# This script uses the partial corpus hash given in a kAFL findings file (e.g.
+# crash_<hash>.log) to find a matching corpus file from metadata given in the
+# metadata/ directory.
+# The procedure may take a few minutes to finish since it's using the mcat.py
+# tool to parse the metadata information.
+##
 
 
-function fail {
-	echo
-	echo -e "$1"
-	echo
-	echo -e "Usage:\n\t$0 <kafl_workdir> <findings_file>"
-	echo
-	exit 1
+####################################
+# Global Variables
+####################################
+WORKDIR=$KAFL_WORKDIR
+FND_NAME=""
+
+####################################
+# Function Definitions
+####################################
+
+# print help text given above in comments
+function help()
+{
+    # find help-start line (indicated by two '#')
+    help_start="`grep -n "^##" "$0" | head -n 1 | awk -F ":" '{print $1}'`"
+    # print only help part
+    tail -n +"$help_start" "$0" | sed -ne '/^#/!q;s/.\{1,2\}//;1d;p'
+    exit
 }
 
-function fail_no_input() {
-	fail "Partial hash from $1 matches hash $2 from $3 but file could not be found in $4."
+# print script usage information given above in comments
+function usage()
+{
+    # find usage line
+    usage_start=$(grep -n "^# Usage" "$0" | awk -F ":" '{print $1}')
+    # print only usage part
+    tail -n +"$usage_start" "$0" | sed -ne '/^#/!q;/^##/q;s/.\{1,2\}//;p'
+    exit
 }
 
-########## MAIN CODE ##########
+# exit with errorcode 1 & print usage.
+# optional argument: print error message.
+function fatal()
+{
+    [[ -n "$1" ]] && echo "Error: $1"; echo
+    usage >&2
+    exit 1
+}
+
+# only print if VERBOSE flag is set
+function verbose_print()
+{
+    [[ $VERBOSE -eq 1 ]] && echo $1
+}
+
+
+function fatal_no_corpus() {
+    fatal "Partial hash from $1 matches hash $2 from $3 but payload could not be found in $4."
+}
+
+
+
+####################################
+# Main()
+####################################
 
 # check if findings file exists
 # grep partial hash from findings file
 # extract readable metadata info from metadata files & remember name of metadata file (same name as corpus)
 # if partial hash found in metadata content then return metadata file name
 
+# argument parsing
+POSITIONAL_ARGS=()
+while [[ "$#" -gt 0 ]]
+do
+    case "$1" in
+        '-h'|'--help')
+            help
+            ;;
+        '-w')
+            [[ -z "$2" ]] && fatal "Missing parameter WORKDIR"
+            WORKDIR="$2"
+        	shift   # past argument
+            shift   # past value
+            ;;
+        '-v')
+            VERBOSE=1
+            shift   # past argument
+            ;;
+        -*|--*)
+            fatal "Unknown option $1"
+            ;;
+        *)
+            POSITIONAL_ARGS+=("$1") # save positional arg
+			[[ -z "$1" ]] && fatal "Missing parameter FILE"
+			FND_NAME=$(basename "$1")
+            shift   # past argument
+            ;;
+    esac
+done
+set -- "${POSITIONAL_ARGS[@]}"  # restore positional parameters
+
 # test if in KAFL environment
-[[ -z "$BKC_ROOT" ]] && fail "Could not find BKC_ROOT. Verify that kAFL environment is set up."
-[[ -z "$KAFL_ROOT" ]] && fail "Could not find KAFL_ROOT. Verify that kAFL environment is set up."
+[[ -z "$BKC_ROOT" ]] && fatal "Could not find BKC_ROOT. Verify that kAFL environment is set up."
+[[ -z "$KAFL_ROOT" ]] && fatal "Could not find KAFL_ROOT. Verify that kAFL environment is set up."
 
 
-# command line parsing
-[[ $# -eq 2 ]] || fail "Missing arguments."
+# test if necessary directories exist
+LOGS=$(realpath $WORKDIR/logs)
+CORPUS_BASE=$(realpath $WORKDIR/corpus)
+CORPUS=$(realpath $CORPUS_BASE/$FND_TYPE)
+METADATA=$(realpath $WORKDIR/metadata)
+[[ -d $WORKDIR ]] || fatal "Could not find kafl working directory $WORKDIR"
+[[ -d $LOGS ]] || fatal "Could not find logs/ folder in $WORKDIR."
+[[ -d $CORPUS_BASE ]] || fatal "Could not find corpus/ folder in $WORKDIR."
+[[ -d $METADATA ]] || fatal "Could not find metadata/ folder in $WORKDIR."
 
-KAFL_WORKDIR=$(realpath $1)	# kAFL work dir with logs/, corpus/ & metdadata/ folders
-FND_NAME=$(basename $2)		# Name of the findings file (e.g. crash_xxxxx.log)
-
-FND_TYPE=$(echo $FND_NAME | sed 's/_.*//')	# type of finding (crash/kasan/timeout)
-LOGS=$KAFL_WORKDIR/logs
-CORPI_BASE=$KAFL_WORKDIR/corpus
-CORPI=$CORPI_BASE/$FND_TYPE
-METADATA=$KAFL_WORKDIR/metadata
 FND_PATH=$LOGS/$FND_NAME
-
-[[ -d $LOGS ]] || fail "Could not find logs/ folder in $KAFL_WORKDIR."
-[[ -d $CORPI_BASE ]] || fail "Could not find corpus/ folder in $KAFL_WORKDIR."
-[[ -d $METADATA ]] || fail "Could not find metadata/ folder in $KAFL_WORKDIR."
-[[ -f $FND_PATH ]] || fail "Could not find findings file $FND_NAME in $LOGS."
-[[ -d $CORPI ]] || fail "Could not find $FND_TYPE/ folder in $CORPUS_BASE."
-
+FND_TYPE=$(echo $FND_NAME | sed 's/_.*//')  # type of finding (crash/kasan/timeout)
+[[ -f $FND_PATH ]] || fatal "Could not find findings file $FND_NAME in $LOGS."
+[[ -d $CORPUS ]] || fatal "Could not find $FND_TYPE/ folder in $CORPUS_BASE."
 
 # grep partial hash from findings file
 FND_HASH=$(echo $FND_NAME | sed 's/.*_//' | tr -d ".log" )
+# echo "$FND_HASH"
 
-# extract readable metadata from metadata files & match corpus hash to node
+# extract readable info from metadata files & match corpus hash to node
 for node in $(ls $METADATA)
 do
-	PAYLOAD=$(echo $node | sed 's/node/payload/')
-	CORPUS_HASH=$(mcat.py $METADATA/$node | grep "hash" | cut -c 20-35)
-	# echo "$node: $CORPUS_HASH"
-	
-	if [[ $CORPUS_HASH == *"$FND_HASH"* ]]
-	then
-		[[ -f $CORPI/$PAYLOAD ]] || fail_no_input $FND_NAME $CORPUS_HASH $PAYLOAD $CORPI
-		echo "$PAYLOAD: $CORPUS_HASH ($CORPI/$PAYLOAD)"
-		exit 0
-	fi
+    # echo "checking $node"
+    PAYLOAD=$(echo $node | sed 's/node/payload/')
+    CORPUS_HASH=$(mcat.py $METADATA/$node | grep "hash" | cut -c 20-35)
+    
+    # echo "$CORPUS_HASH"
+    if [[ $CORPUS_HASH == *"$FND_HASH"* ]]
+    then
+        # echo "matching hash found: $PAYLOAD"
+        match=$(find $CORPUS_BASE -type f -name $PAYLOAD)
+        [[ -z "$match" ]] && fatal_no_corpus $FND_NAME $CORPUS_HASH $PAYLOAD $CORPUS
+        # echo "$PAYLOAD: $CORPUS_HASH ($CORPUS/$PAYLOAD)"
+        echo "$match"
+        exit 0
+    fi
 done
 
-fail_no_input $FND_NAME $CORPUS_HASH $PAYLOAD $CORPI
+echo "loop done"
+fatal_no_corpus $FND_NAME $CORPUS_HASH $PAYLOAD $CORPUS
