26525e1f8b0322e3a4df47bc16af7a64b72b6a40
hemx0147 <rnx781@gmail.com>
2022-11-07 08:18:58 +0100

    find io fns: change default output to fn names only
    add verbose option for better readability

--- code-locations/find_codeql_io_functions.sh
+++ code-locations/find_codeql_io_functions.sh
@@ -4,12 +4,12 @@
 # Find & print occurences of IO Read/Write functions in given directory.
 #
 # Usage: find_io_functions.sh SEARCHDIR
+#        find_io_functions.sh -v SEARCHDIR
 #
+# @param -v         print verbose output
 # @param SEARCHDIR  The directory where the search should be conducted
 ##
 
-# TODO: change output to only show function names
-# TODO: add verbose option that shows more readable output
 
 find_occurences() {
     search_dir="$1"
@@ -24,22 +24,35 @@ function fatal()
 {
     [[ -n "$1" ]] && echo "Error: $1"; echo
     echo "Usage: find_io_functions.sh SEARCHDIR" >&2
+    echo "       find_io_functions.sh -v SEARCHDIR" >&2
     exit 1
 }
 
+# only print if VERBOSE flag is set
+function verbose_print()
+{
+    [[ $VERBOSE -eq 1 ]] && echo "$1"
+}
 
 
 ### MAIN ###
+[[ "$#" -lt 1 || "$#" -gt 2 ]] && fatal "invalid number of input arguments (expected 1 or 2, given $#)"
 
-[[ "$#" -ne 1 ]] && fatal "invalid number of input arguments (expected 1, given $#)"
+VERBOSE=0
+if [[ "$1" -eq "-v" ]]
+then
+    VERBOSE=1
+    [[ -z "$2" ]] && fatal "missing argument: SEARCHDIR" || SEARCH_DIR="$2"
+else
+    SEARCH_DIR="$1"
+fi
 
-SEARCH_DIR="$1"
 [[ -d "$SEARCH_DIR" ]] || fatal "invalid path to SEARCHDIR $SEARCH_DIR"
 
 # search for R/W functions for different I/O components
 for component in 'Cr' 'Msr' 'Mmio' 'Pio' 'PciIo' 'VirtIo'
 do
-    echo "### ${component} ###"
+    verbose_print "### ${component} ###"
     for action in 'Read' 'Write'
     do
         fname1="${action}${component}"
@@ -48,5 +61,7 @@ do
         fname2="${component}${action}"
         find_occurences $SEARCH_DIR $fname2
     done
-    echo
+
+    # print empty line for better readability
+    verbose_print ""
 done
