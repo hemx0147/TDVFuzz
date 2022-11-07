838e75fe796b51dddc7acdc08e401733b152969b
hemx0147 <rnx781@gmail.com>
2022-11-07 07:34:56 +0100

    find io fns: add proper description/help text
    remove unnecessary function
    
    some small fixes

--- code-locations/find_codeql_io_functions.sh
+++ code-locations/find_codeql_io_functions.sh
@@ -1,46 +1,52 @@
-#!/bin/bash
+#!/bin/env bash
 
 ##
 # Find & print occurences of IO Read/Write functions in given directory.
 #
-# @param search_dir	The directory where the search should be conducted
+# Usage: find_io_functions.sh SEARCHDIR
+#
+# @param SEARCHDIR	The directory where the search should be conducted
 ##
 
+# TODO: change output to only show function names
+# TODO: add verbose option that shows more readable output
 
 find_occurences() {
-	search_dir=$1
-	fname=$2
-	find $search_dir -name "*.h" -exec grep "^[[:alnum:]]*${fname}[[:alnum:]]*" {} \; | tr -d ' (' | sort -r | uniq
+	search_dir="$1"
+	fname="$2"
+	find "${search_dir}" -name "*.h" -exec grep "^[[:alnum:]]*${fname}[[:alnum:]]*" {} \; | tr -d ' (' | sort -r | uniq
 }
 
-print_if_output() {
-	to_print=$1
-	output=$2
-	if [ -n "$output" ]
-	then
-		echo "$to_print"
-		echo "$output"
-		echo
-	fi
+
+# exit with errorcode 1 & print usage.
+# optional argument: print error message.
+function fatal()
+{
+    [[ -n "$1" ]] && echo "Error: $1"; echo
+	echo "Usage: find_io_functions.sh SEARCHDIR" >&2
+    exit 1
 }
 
 
 
 ### MAIN ###
 
-search_dir="./kafl.edk2"
-[[ -n "$1" ]] && search_dir=$1
+[[ "$#" -ne 1 ]] && fatal "invalid number of input arguments (expected 1, given $#)"
 
+SEARCH_DIR="$1"
+[[ -d "$SEARCH_DIR" ]] || fatal "invalid path to SEARCHDIR $SEARCH_DIR"
+
+# search for R/W functions for different I/O components
 for component in 'Cr' 'Msr' 'Mmio' 'Pio' 'PciIo' 'VirtIo'
 do
 	echo "### ${component} ###"
 	for action in 'Read' 'Write'
 	do
 		fname1="${action}${component}"
-		find_occurences $search_dir $fname1
+		find_occurences $SEARCH_DIR $fname1
 
 		fname2="${component}${action}"
-		find_occurences $search_dir $fname2
+		find_occurences $SEARCH_DIR $fname2
 	done
 	echo
 done
